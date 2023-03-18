4e1992c6255f115296c163ff49ebbc0980e88139
Chinvoxel <1350106846@qq.com>
2023-03-18 20:49:04 +0800

    chore: 基础配置的构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
+  },
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@use "@/styles/variables.scss" as *;`
+      }
+    }
+  }
 })
